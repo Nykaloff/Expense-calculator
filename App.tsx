f03f515576b5cfc08f732e35a87f3bd2aef2d047
Niukalov <niukalov@gmail.com>
2022-06-28 17:17:23 +0300

    first

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
