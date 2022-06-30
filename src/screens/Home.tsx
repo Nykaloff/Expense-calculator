3f23d441bc186aa0fda8668707ed85cf01fbbe5f
Niukalov <niukalov@gmail.com>
2022-06-30 18:10:35 +0300

    structure

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
