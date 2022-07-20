962911af3c13296abaaee4f87fba1fd773f68cba
Niukalov <niukalov@gmail.com>
2022-07-20 23:38:34 +0300

    for Alex-dev

---
+++ src/assets/components/selectClientProcedure.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const SelectClientProcedure: React.FC = () => {
+  return (
+    <View>
+      <Text>Сделай тут выборку из клиентов</Text>
+    </View>
+  );
+};
